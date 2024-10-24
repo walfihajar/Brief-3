b2dad16cd743a40d71005edab9bec0baee736d04
ChehAchraf <achrafchehboun@outlook.com>
2024-10-24 20:56:31 +0100

    adding the loan page, and add the script thet calculate the loan

---
+++ js/loan-calc.js
@@ -0,0 +1,12 @@
+let form = document.getElementById('form');
+let loan_reason = document.getElementById('loan-reason');
+let jobs = document.getElementById('jobs');
+let mount = document.getElementById('mount'); // Fixed the ID typo
+let amount = document.getElementById('amount');
+let result = document.getElementById('result');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let res = mount.value * 0.6 * amount.value + 500;
+    result.innerHTML = res.toFixed(2)
+});
