03bf720ea5069ddf8e2601ae374a9061deb8c3ff
wasvlgs <wassimwassim.ka1@gmail.com>
2024-10-25 20:18:34 +0100

    update scripts

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+
+// Navbar
+
+var openList = document.getElementById("openList");
+var closeList = document.getElementById("closeList");
+var List = document.getElementById("List");
+
+
+
+openList.onclick = ()=>{
+    List.style.width = "100%";
+}
+
+closeList.onclick = ()=>{
+    List.style.width = "0%";
+}
+
+    
